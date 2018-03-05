333045c658f6199a4348ff395078f635a5fa8f69
lswh <labao.hm@gmail.com>
2018-03-05 17:31:32 +0800

    Ads Txt Detector for Listing URLs in a Single Dir

--- adstxtdetect.py
+++ adstxtdetect.py
@@ -1,3 +1,26 @@
-import pandas as pd
-import scrapy as sc
+import os
 
+
+#Writes all the URLs in the folder into a list of filenames
+dirListing = os.listdir(os.getcwd())
+
+
+#Initialize list of files
+editFiles = []
+
+for item in dirListing:
+	if ".html" in item:
+		editFiles.append(item)
+print editFiles
+
+# Checks for common words DIRECT and RESELLER present on ads.txt file. 
+for website in editFiles:
+	with open(website) as f:
+	   for line in f:
+	       if 'DIRECT' in line:
+	           print f.name
+	           break
+	       elif 'RESELLER' in line:
+	      			print f.name
+	      			break
+	   f.close
