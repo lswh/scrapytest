d3915ef1131b426f42135e71b558aa64e2fcd1f5
lswh <labao.hm@gmail.com>
2018-03-29 19:46:22 +0800

    holy week ds

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("data2.txt") as file:
+        with open("xhr") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
