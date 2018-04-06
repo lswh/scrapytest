69aa8130f986030cf453657a624124587920a3ea
H <helen@monetizemore.com>
2018-04-06 00:49:42 +0000

    xgl

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xgo") as file:
+        with open("xgl") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
