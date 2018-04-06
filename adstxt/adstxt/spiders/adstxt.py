50097aded83d647b49e19e38b07c45651f508de1
lswh <labao.hm@gmail.com>
2018-04-06 15:28:38 +0800

    xge

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xha") as file:
+        with open("xge") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
