c927ede6b28838b363b5adae4b94c27b844b35d5
H <helen@monetizemore.com>
2018-04-09 12:15:11 +0000

    xep

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xfe") as file:
+        with open("xep") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
