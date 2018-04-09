baceae0eb1cc1b6094d54c43bc66c53e64307cbf
H <helen@monetizemore.com>
2018-04-09 23:51:58 +0000

    xee

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xes") as file:
+        with open("xee") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
