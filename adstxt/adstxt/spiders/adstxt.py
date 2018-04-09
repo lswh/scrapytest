250d236fd312278d9c802a383fe50a96e2c8376b
Jeng Labao-Barrameda <jeng@SuburbanMom.local>
2018-04-09 22:15:54 +0800

    xel

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xem") as file:
+        with open("xel") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
