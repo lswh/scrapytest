26d87796fe0cc307f670420db305ff7c6f20bc3f
lswh <labao.hm@gmail.com>
2018-04-10 08:39:23 +0800

    xdz

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xfw") as file:
+        with open("xdz") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
