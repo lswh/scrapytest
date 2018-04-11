01652aa6833ceff27d72534d95d6fce5275ef453
H <helen@monetizemore.com>
2018-04-11 00:45:00 +0000

    xcu

--- adstxt/adstxt/spiders/adstxt.py
+++ adstxt/adstxt/spiders/adstxt.py
@@ -6,7 +6,7 @@ class AdstxtSpider(scrapy.Spider):
     def  start_requests(self):
 
         urls=[]
-        with open("xdk") as file:
+        with open("xcu") as file:
             for line in file: 
                 line = line.strip()
                 urls.append('http://'+line+'/ads.txt') 
